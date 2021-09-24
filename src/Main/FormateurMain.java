6034a9f6df9d3efc9da12ff8ac6a35a2079ef2ef
imanesafer <89776467+imanesafer@users.noreply.github.com>
2021-09-24 17:12:49 +0200

    b

--- src/Main/FormateurMain.java
+++ src/Main/FormateurMain.java
@@ -1,10 +1,29 @@
 package Main;
 
+import ClassesTest.Testclasses;
+import QuatreClasseArrayList.SessionList;
+import QuatrecClasses.Session;
+
 public class FormateurMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 String nom =;
+	     String prénom =;
+		String specialite =;
+		 String grade = ;
+		Testclasses va = new Testclasses();
 
+		if (va.verifChaine(debut) && va.verifChaine(fin) && va.verifChaine(titre_formation) && va.verifChaine(lieu)
+				&& va.verifChaine(mode)) {
+			Session ad = new Session(debut, fin, titre_formation, lieu, mode);
+			SessionList.listSessions.add(ad);
+			SessionList.showList();
+
+		} else {
+			System.out.println("mode incorrect");
+		}
 	}
+}
 
 }
--- src/Main/SessionMain.java
+++ src/Main/SessionMain.java
@@ -8,31 +8,21 @@ public class SessionMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String debut ="05/07/2021" ;
-		 String fin ="24/09/2021" ;
-		String titre_formation= "testeur d'application";
-		String lieu= "Sogeti";
-	 String mode="présentiel";
-		 Testclasses va = new Testclasses();
-	
-		 if (va.verifChaine(debut) && va.verifChaine(fin)&& va.verifChaine(titre_formation)&& va.verifChaine(lieu)&& va.verifChaine(lieu) ) {
-				if (va.validId(id)) {
-					Session ad = new Session();
-					Session ad2 = new Session(10, nom,departement);
-					Session ad3 = new Session(-3, nom,departement);
+		String debut = "05/07/2021";
+		String fin = "24/09/2021";
+		String titre_formation = "testeur d'application";
+		String lieu = "Sogeti";
+		String mode = "présentiel";
+		Testclasses va = new Testclasses();
 
-					SessionList.listSessions.add(ad);
-					SessionList.listSessions.add(ad2);
-					SessionList.listSessions.add(ad3);
-					SessionList.showList();
+		if (va.verifChaine(debut) && va.verifChaine(fin) && va.verifChaine(titre_formation) && va.verifChaine(lieu)
+				&& va.verifChaine(mode)) {
+			Session ad = new Session(debut, fin, titre_formation, lieu, mode);
+			SessionList.listSessions.add(ad);
+			SessionList.showList();
 
-				} else {
-					System.out.println("id doit etre superieur à 0!");
-				}
-
-			} else
-				System.out.println("l'un des champs est vide!");
-
+		} else {
+			System.out.println("mode incorrect");
 		}
-
 	}
+}
