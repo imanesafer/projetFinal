02a5de826fea8138545c20402ce662ae6c8b754b
imanesafer <89776467+imanesafer@users.noreply.github.com>
2021-09-24 18:06:23 +0200

    d

--- src/Main/FormationMain.java
+++ src/Main/FormationMain.java
@@ -1,27 +1,25 @@
 package Main;
 
-import ArrayList.FormationArrayList;
 import ClassesTest.Testclasses;
 import QuatreClasseArrayList.FormationList;
 import QuatrecClasses.Formation;
-import covid_19.Formation;
 
 public class FormationMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String titre = "formation orsys";
-		String Formation ="LAKHDHAR Khalil";
+		String Formation = "LAKHDHAR Khalil";
 		int prix = 250;
 		int nb_heures = 10;
 		Testclasses va = new Testclasses();
 
 		if (va.verifChaine(titre) && va.verifChaine(Formation)) {
-			if (va.validPrix() && va.validNb_heures(nb_heures))) {
-				Formation ad = new Formation(titre,Formation,prix,nb_heures);
-				Formation ad1 = new Formation(titre,Formation,prix,nb_heures);
-				Formation ad2 = new Formation(titre,Formation,prix,nb_heures);
-				
+			if (va.validPrix(prix) && va.validNb_heures(nb_heures)) {
+				Formation ad = new Formation(titre, Formation, 0, 10);
+				Formation ad1 = new Formation(titre, Formation, 100, 11);
+				Formation ad2 = new Formation(titre, Formation, 250, 40);
+
 				FormationList.listFormations.add(ad);
 				FormationList.listFormations.add(ad1);
 				FormationList.listFormations.add(ad2);
@@ -31,9 +29,8 @@ public class FormationMain {
 			}
 
 		} else
-			System.out.println("prix ou nb_heures vide!");
+			System.out.println("autre paramètres pas correct!");
 
 	}
 
 }
-			
